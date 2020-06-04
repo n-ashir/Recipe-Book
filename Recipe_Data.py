72cb1466dc990e5282101e26221d969dfa166a32
n-ashir <66327606+n-ashir@users.noreply.github.com>
2020-06-04 12:38:03 +0200

    Add files via upload

---
+++ Recipe_Data.py
@@ -0,0 +1,305 @@
+
+breakfast_and_brunch = {
+'Puff Pastry Waffles':
+[
+['1 (17.3 ounce) package frozen puff pastry, thawed','cooking spray'],
+['338 calories, 23.4 g fat, 27.7 g carbohydrates, 4.5 g protein, 0 mg cholesterol, 153 mg sodium'],
+['''1.Line a cutting board with parchment paper. 
+Unfold puff pastry onto cutting board. Cut each sheet into 4 equal squares.
+2.Preheat a waffle iron according to manufacturer's instructions. Grease with cooking spray.
+3.Place one puff pastry square in the preheated waffle iron, cook until golden brown, 3 to 5 minutes. 
+Repeat with remaining puff pastry squares.''']
+],
+'Good Old Fashioned Pancakes':
+[
+['1/2 cups all-purpose flour', '1/2 teaspoons baking powder','1 teaspoon salt','1 tablespoon white sugar',
+ '1/4 cups milk','1 egg','3 tablespoons butter, melted'],
+['158 calories, 5.9 g fat, 21.7 g carbohydrates, 4.5 g protein, 38 mg cholesterol, 504 mg sodium'],
+['''1.In a large bowl, sift together the flour, baking powder, salt and sugar. 
+Make a well in the center and pour in the milk, egg and melted butter, mix until smooth.
+2.Heat a lightly oiled griddle or frying pan over medium high heat. Pour or scoop the batter onto the griddle, 
+using approximately 1/4 cup for each pancake. Brown on both sides and serve hot.''']
+]
+}
+desserts = {
+'Fresh Berry Fool':
+[
+['1 cup fresh raspberries','1 cup fresh blackberries, broken in half','1 cup fresh strawberries, hulled and quartered',
+ '1 teaspoon lemon zest','1 teaspoon freshly squeezed lemon juice','1/4 cup white sugar','1 cup chilled heavy cream',
+ '1/4 cup creme fraiche','1 tablespoon white sugar','1/4 teaspoon pure vanilla extract','1 cup crushed shortbread cookies'],
+['646 calories, 41.7 g fat, 64.8 g carbohydrates, 6.3 g protein, 113 mg cholesterol, 287 mg sodium'],
+['''1.Place raspberries, blackberries, and strawberries in a bowl. Add lemon zest, 
+lemon juice, and 1/4 cup sugar. Stir until sugar is mixed with fruit and juices begin to form. 
+Cover and refrigerate until chilled, about 1 hour.
+2.Place heavy cream and creme fraiche in a cold bowl. Add 1 tablespoon sugar and vanilla extract. 
+Whisk until mixture has soft peaks and sharp lines, 5 to 8 minutes.
+3.Place some cookie crumbs in the bottom of each serving dish. Spoon in some of the fruit and a layer of whipped cream. 
+Repeat layers, ending with a sprinkle of cookie crumbs.''']
+],
+'Too Much Chocolate Cake':
+[
+['1 (5.9 ounce) package instant chocolate pudding mix','1 cup sour cream','1 cup vegetable oil','4 eggs',
+ '½ cup warm water','2 cups semisweet chocolate chips'],
+['600 calories, 38.6 g total fat, 79 mg cholesterol, 550 mg sodium. 60.9 g carbohydrates, 7.6 g protein'],
+['''1.Preheat oven to 350 degrees F (175 degrees C).
+2.In a large bowl, mix together the cake and pudding mixes, sour cream, oil, beaten eggs and water. 
+Stir in the chocolate chips and pour batter into a well greased 12 cup bundt pan.
+3.Bake for 50 to 55 minutes, or until top is springy to the touch and a wooden toothpick inserted comes out clean. 
+Cool cake thoroughly in pan at least an hour and a half before inverting onto a plate If desired, dust the cake with 
+powdered sugar.''']
+]
+}
+dinners = {
+'Super Simple Salmon':
+[
+['1 tablespoon garlic powder','1 tablespoon dried basil','1/2 teaspoon salt','4 (6 ounce) salmon','2 tablespoons butter',
+ '4 lemon wedges'],
+['304 calories, 15.7 g fat, 3.3 g carbohydrates, 36.6 g protein, 90 mg cholesterol, 402 mg sodium.'],
+['''1.Stir together the garlic powder, basil, and salt in a small bowl, rub in equal amounts onto the salmon fillets.
+2.Melt the butter in a skillet over medium heat, cook the salmon in the butter until browned and flaky, 
+about 5 minutes per side. Serve each piece of salmon with a lemon wedge.''']
+],
+'Rosemary Ranch Chicken Kabobs':
+[
+['½ cup olive oil','½ cup ranch dressing','3 tablespoons Worcestershire sauce','1 tablespoon minced fresh rosemary',
+ '2 teaspoons salt'
+ '1 teaspoon lemon juice','1 teaspoon white vinegar','¼ teaspoon ground black pepper, or to taste',
+ '1 tablespoon white sugar, or to taste',
+ '5 eaches skinless, boneless chicken breast halves - cut into 1 inch cubes'],
+['377.7 calories, 19.9 g protein, 4.8 g carbohydrates, 59.2 mg cholesterol, 1097.2 mg sodium.'],
+['''1.In a medium bowl, stir together the olive oil, ranch dressing, 
+Worcestershire sauce, rosemary, salt, lemon juice, white vinegar, pepper, and sugar. 
+Let stand for 5 minutes. Place chicken in the bowl, and stir to coat with the marinade. Cover and refrigerate for 30 minutes.
+2.Preheat the grill for medium-high heat. Thread chicken onto skewers and discard marinade.
+3.Lightly oil the grill grate. Grill skewers for 8 to 12 minutes, or until the chicken is no longer pink in the center, 
+and the juices run clear.''']
+]
+}
+lunch = {
+'Fruited Curry Chicken Salad':
+[
+['4 skinless, boneless chicken breast halves - cooked and diced','1 stalk celery, chopped','½ onion, chopped',
+ '1 small apple - peeled, cored and chopped',
+ '⅓ cup golden raisins','⅓ cup seedless green grapes, halved','½ cup chopped toasted pecans',
+ '⅛ teaspoon ground black pepper','½ teaspoon curry powder','¾ cup mayonnaise'],
+['306.2 calories, 15 g protein, 11.5 g carbohydrates, 44.4 mg cholesterol, 152.6 mg sodium. '],
+['''In a large salad bowl combine the chicken, celery, onion, apple, raisins, 
+grapes, pecans, pepper, curry powder and mayonnaise. Mix all together, tossing to coat. Salad is ready to serve!''']
+],
+'Summer Corn Salad':
+[
+['6 ears corn, husked and cleaned','3 large tomatoes, diced','1 large onion, diced','1/4 cup chopped fresh basil',
+ '1/4 cup olive oil',
+ '2 tablespoons white vinegar','salt and pepper to taste'],
+['305 calories, 15.6 g fat, 42.8 g carbohydrates, 6.2 g protein, 0 mg cholesterol, 9 mg sodium.'],
+['''1.Bring a large pot of lightly salted water to a boil. 
+Cook corn in boiling water for 7 to 10 minutes, or until desired tenderness. 
+Drain, cool, and cut kernels off the cob with a sharp knife.
+2.In a large bowl, toss together the corn, tomatoes, onion, basil, oil, vinegar, salt and pepper. Chill until serving.''']
+]
+}
+
+
+def print_recipe(type_of_m):
+
+    if type_of_m == 1:
+        return print('\n', 'Breakfast and Brunch Recipes! ')
+
+    elif type_of_m == 2:
+        return print('\n', 'Dessert Recipes')
+
+    elif type_of_m == 3:
+        return print('\n', 'Dinner Recipes! ')
+
+    elif type_of_m == 4:
+        return print('\n', 'Lunch Recipes!')
+
+
+def option(options):
+
+    if options == 1:
+        print('\n add new Recipe!')
+
+    elif options == 2:
+        print('\n update Recipes!')
+
+    elif options == 3:
+        print('\n search Recipe!')
+
+    elif options == 4:
+        print('\n delete Recipe!')
+
+def dictions (type_of_m, name_of_recipe):
+
+    if type_of_m == 1:
+        return breakfast_and_brunch[name_of_recipe]
+
+    elif type_of_m == 2 :
+        return desserts[name_of_recipe]
+
+    elif type_of_m == 3 :
+        return dinners[name_of_recipe]
+
+    elif type_of_m == 4 :
+        return lunch[name_of_recipe]
+
+def dictions_(type_of_m):
+
+    if type_of_m == 1:
+        return breakfast_and_brunch
+
+    elif type_of_m == 2 :
+        return desserts
+
+    elif type_of_m == 3 :
+        return dinners
+
+    elif type_of_m == 4 :
+        return lunch
+
+def loops (type_of_m):
+    print('\nName of Recipes in the Book:\n')
+
+    if type_of_m == 1:
+        for key in breakfast_and_brunch:
+            print(key, end=', ')
+        return
+
+    elif type_of_m == 2:
+        for key in desserts:
+            print(key, end=', ')
+        return
+
+    elif type_of_m == 3:
+        for key in dinners:
+            print(key, end=', ')
+        return
+
+    elif type_of_m == 4:
+        for key in lunch:
+            print(key, end=', ')
+        return
+
+def update_recipe(updt):
+
+    if updt == 0 :
+       print('''\n Please input comma (,) after each ingredients:
+                   for example: 2 cups vanilla yogurt ,1 cup granola ''')
+
+       exist_ = my_dict[updt]
+
+       print('\nHere are exist Ingredients: ', *exist_)
+
+       ingredients = input('\n Please input here ingredients: ', )
+       ingredients = ingredients.split(',')
+
+       my_dict[updt] = ingredients
+       prn = my_dict[updt]
+
+       return print('\nRecipe Name: ' + name_of_recipe, ' after updating Ingredients ', *prn)
+
+    elif updt == 1:
+        print('''\n Please input comma (,) after each Nutrition:        
+                        for example: 305 calories, 15.6 g fat, 42.8 g carbohydrates ''')
+
+        exist_ = my_dict[updt]
+        print('Here are exist Nutrition: ', *exist_)
+
+        calories = input('Please input heare Nutrition Facts! ', )
+        calories = calories.split(',')
+        my_dict[updt] = calories
+
+        prn = my_dict[updt]
+        return print('Recipe Name: '+ name_of_recipe, ' after updating Nutrition ', *prn)
+
+    elif updt == 2:
+        exist_ = my_dict[updt]
+        print('Here are exist Direction: ', *exist_)
+
+        to_prepare = input('Please input heare Direction! ', )
+        my_dict[updt] = [to_prepare]
+
+        prn = my_dict[updt]
+        return print('\nRecipe Name: ' + name_of_recipe, ' after updating Direction! ', *prn)
+
+def option_step_two (options):
+
+    if  options == 1:
+        name_of_recipe = input("\n Please input recipe\'s name! ")
+        print('''\n Please input comma (,) after each ingredients:
+            for example: 2 cups vanilla yogurt ,1 cup granola ''')
+
+        ingredients = input('\n Please input here ingredients: ', )
+        ingredients = ingredients.split(',')
+
+        my_dict = dictions(type_of_m)
+        my_dict = [[], [], []]
+        my_dict[0] = ingredients
+
+        print('''\n Please input comma (,) after each Nutrition:        
+                    for example: 305 calories, 15.6 g fat, 42.8 g carbohydrates ''')
+
+        calories = input('Please input heare Nutrition Facts! ', )
+        calories = calories.split(',')
+        my_dict[1] = calories
+
+        to_prepare = input('Please input heare Direction! ', )
+        my_dict[2] = [to_prepare]
+
+        return print(my_dict)
+
+    elif options == 2:
+          loops(type_of_m)
+          name_of_recipe = input("\n\nPlease input recipe\'s name! ")
+
+          print(
+                 str(name_of_recipe) + ' has three values, Ingredients, Nutritions, Direction. Which one do you wanna update ? ')
+
+          print('''
+              for Ingredients, input 0
+              for Nutritions,  input 1
+              for Directions,  input 2            
+           ''')
+
+          my_dict = dictions(type_of_m, name_of_recipe)
+
+          updt = input('Please choose one of them ', )
+          updt = int(updt)
+          update_recipe(updt)
+
+    elif options == 3:
+          my_dict_ = dictions_(type_of_m)
+          convert = print_recipe(type_of_m)
+          print(convert+':')
+          print(*my_dict_, '', sep=',' , end='.',)
+          print('')
+
+          name_of_recipe = input("\nPlease search your Recipe in The "+convert+'!')
+
+          if name_of_recipe in my_dict_:
+             new_list = my_dict_[name_of_recipe]
+             print('')
+             print(convert+'Ingriedents: ', ','.join(new_list[0]))
+             print(convert+'Nutriotions: ', ','.join(new_list[1]))
+             print(convert+'Direction: ', ','.join(new_list[2]))
+             return
+
+          else:
+             print('')
+             print('Searched Recipe has not in The Recipe Book')
+             return
+
+    elif options == 4:
+          my_dict_ = dictions_(type_of_m)
+          convert = type_of_meal(type_of_m)
+          print(convert + ':')
+          print('Which one do you wanna delete!')
+          print(*my_dict_, '', sep=',', end='.', )
+          print('')
+          return
+          name_of_recipe = input("\nPlease delete Recipe in The " + convert + '!')
+
+          if name_of_recipe in my_dict_:
+             del  my_dict_[name_of_recipe]
+             return print(*my_dict_)
+
